d0a26e6b5035031caae9b747783313fd2d33011e
Abu Azis <muhammadazis801@gmail.com>
2020-05-28 20:13:37 +0700

    06-class-object-pbo-dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,55 +1,27 @@
 import 'dart:io';
 
 void main(List<String> arguments) {
-  List<int> myList = [1, 1, 4, 3, 6, 2, 7];
-  List<int> list = [];
-  List<String> listString = [];
+  PersegiPanjang kotak1, kotak2;
+  double luasKotak1;
 
-  list[0] = 10;
-  int e = list[0];
-  print(e);
+  kotak1 = new PersegiPanjang();
+  kotak1.panjang = 2;
+  kotak1.lebar = 3;
 
-  for (int index = 0; index < list.length; index += 1) {
-    print(index);
+  kotak2 = PersegiPanjang();
+  kotak2.panjang = double.tryParse(stdin.readLineSync());
+  kotak2.lebar = double.tryParse(stdin.readLineSync());
+
+  luasKotak1 = kotak1.hitungLuas();
+
+  print(luasKotak1 + kotak2.hitungLuas());
+}
+
+class PersegiPanjang {
+  double panjang;
+  double lebar;
+
+  double hitungLuas() {
+    return this.panjang * lebar;
   }
-
-  for (int bilangan in list) {
-    print(bilangan);
-  }
-
-  myList.add(10);
-  myList.addAll(list);
-  myList.insert(2, 12);
-  myList.insertAll(5, [12, 13]);
-  myList.remove(12);
-  myList.removeRange(1, 3);
-  myList.removeWhere((number) => number % 2 != 0);
-  list = myList.sublist(3, 6);
-  list.clear();
-
-  // a = 4, b = 5
-  myList.sort((a, b) => b - a);
-  myList.removeWhere((number) => number % 2 == 0);
-  if (myList.every((number) => number % 2 != 0)) {
-    print("semua ganjil");
-  } else {
-    print("tidak semua ganjil");
-  }
-
-  if (myList.contains(6)) {
-    print("betul");
-  }
-
-  if (myList.isEmpty) {
-    print("kosong");
-  }
-
-  Set<int> s;
-  s = myList.toSet();
-
-  listString = myList.map((number) => "angka " + number.toString()).toList();
-
-  list.forEach((bilangan) { 
-    print(bilangan);
-  });
 }
