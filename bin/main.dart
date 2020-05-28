9301e7ef068724b1edabbfee59fa47d1c8ed6685
Abu Azis <muhammadazis801@gmail.com>
2020-05-28 17:06:15 +0700

    04-function-pbo-dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,42 @@
 import 'dart:io';
 
+// double luas_segiempat(double panjang, double lebar) {
+//   return panjang * lebar;
+// }
+
+void sapa_penonton() {
+  print("Hello Penonton");
+}
+
+String say(String from, String message, {String to, String appName}) {
+  return from + " say " + message + ((to != null) ? " to " + to : "") + 
+  ((appName != null) ? " via " + appName : "");
+}
+
+String say2(String from, String message, [String to, String appName = "Whatsapp"]) {
+  return from + " say " + message + ((to != null) ? " to " + to : "") + 
+  ((appName != null) ? " via " + appName : "");
+}
+
+double luas_segiempat(double panjang, double lebar) => panjang * lebar;
+
+int doMathOperator(int number1, int number2, Function(int, int) operator) {
+  return operator(number1, number2);
+}
+
 void main(List<String> arguments) {
 
-  int a, b;
-  a = 10;
-  b = a--;
-  print(a.toString() + " - " + b.toString());
+  Function f;
+  f = luas_segiempat;
+  print(say("Abuza", "Oiii", appName: "Telegram"));
+  print(say2("Abuza", "Oiii", "Doloris"));
+  print(luas_segiempat(3, 2));
+  print(f(3.0, 2.0));
+  print(doMathOperator(1, 2, (a, b) => a * b));
+  double p, l, luas;
 
-  int i = 5;
-  while(i > 0) {
-    print("halo ke " + i.toString());
-    i--;
-  }
+  p = double.tryParse(stdin.readLineSync());
+  l = double.tryParse(stdin.readLineSync());
 
-  print(" ============= ");
-
-  int x = 0;
-  do {
-    print("dododo ke " + x.toString());
-    x += 1;
-  } while (x < 5);
-
-  for (int counter = 9; counter >= 1; counter -= 2) {
-    print("halo");
-  }
+  print(luas_segiempat(p, l));
 }
