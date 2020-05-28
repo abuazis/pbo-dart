32c48ca755be24030b2674ce036b2e2dcc567262
Abu Azis <muhammadazis801@gmail.com>
2020-05-29 06:43:49 +0700

    07-enkapsulasi-pbo-dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,27 +1,20 @@
 import 'dart:io';
+import 'package:my_first_dart_app/persegi_panjang.dart';
 
 void main(List<String> arguments) {
   PersegiPanjang kotak1, kotak2;
   double luasKotak1;
 
   kotak1 = new PersegiPanjang();
-  kotak1.panjang = 2;
-  kotak1.lebar = 3;
+  kotak1.setPanjang(2);
+  kotak1.lebar = -3;
 
   kotak2 = PersegiPanjang();
-  kotak2.panjang = double.tryParse(stdin.readLineSync());
+  kotak2.setPanjang(double.tryParse(stdin.readLineSync()));
   kotak2.lebar = double.tryParse(stdin.readLineSync());
 
-  luasKotak1 = kotak1.hitungLuas();
+  luasKotak1 = kotak1.luas;
 
-  print(luasKotak1 + kotak2.hitungLuas());
-}
-
-class PersegiPanjang {
-  double panjang;
-  double lebar;
-
-  double hitungLuas() {
-    return this.panjang * lebar;
-  }
+  print(luasKotak1 + kotak2.luas);
+  print(kotak1.lebar);
 }
